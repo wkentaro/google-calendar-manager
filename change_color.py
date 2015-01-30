a8bf37aa0d4afc54bc7c3f2bbfb65910dfab7db4
Kentaro Wada <www.kentaro.wada@gmail.com>
2015-01-30 19:16:39 +0900

    add colors config yaml file

--- change_color.py
+++ change_color.py
@@ -3,44 +3,59 @@
 #
 from __future__ import print_function
 import datetime
+import yaml
+import collections
 
 import get_credential
 
 
-def main():
+def get_title_from_summary(summary):
+    contents = summary.split(',')
+
+    titles = []
+    for content in contents:
+        title = content.split(':')[0]
+        titles.append(title)
+
+    return collections.Counter(titles).keys()[0]
+
+
+def change_events_color():
+    # get colors data
+    with open('colors.yml', 'rb') as f:
+        colors_data = yaml.load(f)
+
     service = get_credential.credential("")
     page_token = None
     ids = []
     today = datetime.date.today()
-    change_start = today - datetime.timedelta(days=3)
-    change_end = today + datetime.timedelta(days=3)
+    change_start = today - datetime.timedelta(days=2)
+    change_end = today + datetime.timedelta(days=2)
     while True:
-        events = service.events().list(calendarId="primary",
+        events = service.events().list(
+                calendarId='primary',
                 pageToken=page_token,
-                timeMin=change_start.strftime("%Y-%m-%dT00:00:00+09:00"),
-                timeMax=change_end.strftime("%Y-%m-%dT00:00:00+09:00")).execute()
-        for event in events["items"]:
-            try:
-                summary = event['summary'].lower()
-                if (summary.startswith('study') or 
-                    summary.startswith('english') or
-                    summary.startswith('work')):
-                    event['colorId'] = 9
-                elif summary.startswith('run'):
-                    event['colorId'] = 10
-                elif summary.startswith('pastime'):
-                    event['colorId'] = 3
-                elif summary.startswith('univ'):
-                    event['colorId'] = 6
-                event['summary'] = summary.capitalize()
-                service.events().update(calendarId='primary',
-                    eventId=event['id'], body=event).execute()
-            except KeyError:
-                pass
+                timeMin=change_start.strftime('%Y-%m-%dT00:00:00+09:00'),
+                timeMax=change_end.strftime('%Y-%m-%dT00:00:00+09:00'),
+                ).execute()
+        for event in events['items']:
+            if 'summary' not in event:
+                continue
+            summary = event['summary'].lower()
+            for color, rules in colors_data['color_rules'].items():
+                if summary in rules:
+                    event['colorId'] = colors_data['colors'][color]
+                    break
+            event['summary'] = summary.capitalize()
+            service.events().update(
+                calendarId='primary',
+                eventId=event['id'],
+                body=event,
+                ).execute()
         page_token = events.get('nextPageToken')
         if not page_token:
             break
 
 
 if __name__ == '__main__':
-    main()
+    change_events_color()
